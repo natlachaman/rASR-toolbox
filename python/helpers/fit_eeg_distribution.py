82c2b7642db6d640c1f98b8d0c45e14e0b7535c3
natlachaman <nati_269@hotmail.com>
2020-05-11 21:01:30 +0200

    finished fit_egg_distribution()

--- python/helpers/fit_eeg_distribution.py
+++ python/helpers/fit_eeg_distribution.py
@@ -1,5 +1,8 @@
 from typing import Tuple
 import numpy as np
+from scipy.special import gammaincinv, gamma
+
+from .utils import _histc, _kl_divergence
 
 
 def fit_eeg_distribution(X: np.ndarray, min_clean_fraction: float = .25, max_dropout_fraction: float = .1,
@@ -57,4 +60,71 @@ def fit_eeg_distribution(X: np.ndarray, min_clean_fraction: float = .25, max_dro
         estimated shape parameter of the generalized Gaussian clean EEG distribution (optional)
 
     """
-    pass
+    # sort data so we can access quantiles directly
+    X = np.sort(X)
+    n = len(X)
+    beta = np.arange(beta[0], beta[1], step=beta[2])
+    quants = np.array(quants)
+
+    # calc z bounds for the truncated standard generalized Gaussian pdf and pdf rescaler
+    zbounds = np.zeros((len(beta), len(quants)))
+    rescale = np.zeros_like(beta)
+    for b in range(len(beta)):
+        zbounds[b] = np.sign(quants - 1 / 2) * gammaincinv(np.sign(quants - 1 / 2) * (2 * quants - 1),
+                                                           1 / beta[b]) ** (1 / beta[b])
+        rescale[b] = beta[b] / (2 * gamma(1 / beta[b]))
+
+    # determine the quantile-dependent limits for the grid search
+    lower_min = np.min(quants)                     # we can generally skip the tail below the lower quantile
+    max_width = np.diff(quants)                    # maximum width is the fit interval if all data is clean
+    min_width = min_clean_fraction * max_width     # minimum width of the fit interval, as fraction of data
+
+    # get matrix of shifted data ranges
+    a = np.arange(int(np.round(n * max_width)))
+    b = np.round(np.arange(lower_min, lower_min + max_dropout_fraction, step=step_sizes[0]) * n)
+    X = X[a + b]
+    X1 = X[0, :]
+    X -= X1
+
+    # for each interval width...
+    opt_val = np.Inf
+    opt_beta = 0
+    opt_bounds = np.zeros((1, len(quants)))
+    opt_lu = np.zeros((1, 2))
+
+    for m in np.round(np.arange(min_width, max_width, step=step_sizes[1]) * n)[::-1]:
+
+        # scale and bin the data in the intervals
+        nbins = np.round(3 * np.log2(1 + m / 2))
+        H = X[:m, :] * (nbins / X[m, :])
+        q = _histc(X, nbins) + 0.01
+
+        # for each shape value...
+        for b in range(len(beta)):
+            bounds = zbounds[b, :]
+
+            # evaluate truncated generalized Gaussian pdf at bin centers
+            x = bounds[0] + np.arange(0.5, nbins-0.5) / nbins * np.diff(bounds)
+            p = np.exp(-np.abs(x) ** beta[b]) * rescale[b]
+            p /= np.sum(p)
+
+            # calc KL divergences
+            kl = _kl_divergence(p, q)
+
+            # update optimal parameters
+            min_val, idx = np.min(kl), np.argmin(kl)
+            if min_val < opt_val:
+                opt_val = min_val
+                opt_beta = beta[b]
+                opt_bounds = bounds
+                opt_lu = np.r_[X1[idx], X1[idx] + X[m, idx]]
+
+    # recover distribution parameters at optimum
+    alpha = (opt_lu[1] - opt_lu[0]) / np.diff(opt_bounds)
+    mu = opt_lu[0] - opt_bounds[0] * alpha
+    beta = opt_beta
+
+    # calculate the distribution's standard deviation from alpha and beta
+    sigma = np.sqrt((alpha ** 2) * gamma(3 / beta) / gamma(1 / beta))
+
+    return mu, sigma, alpha, beta
--- python/helpers/utils.py
+++ python/helpers/utils.py
@@ -21,3 +21,17 @@ def _sliding_window(array, window, steps=1, axis=1):
 
     # Window samples
     return np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
+
+
+def _histc(x, nbins):
+    """Histogram count (bin-centered). As implemented in histc in Matalb."""
+    # bin_edges = np.r_[-np.Inf, 0.5 * (bin_centers[:-1] + bin_centers[1:]),
+    #     np.Inf]
+    bin_edges = np.r_[np.arange(nbins - 1), np.Inf]
+    counts, edges =  np.histogram(x, bin_edges)
+    return counts
+
+
+def _kl_divergence(p, q):
+    """KL divergence"""
+    return np.sum(np.where(p != 0, p * np.log(p / q), 0))
