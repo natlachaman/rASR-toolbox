d249d606178ff193f756f4bd83a4dedfd9d1c653
natlachaman <nati_269@hotmail.com>
2020-05-16 15:45:25 +0200

    added docstrings to karcher means

--- python/helpers/positive_definite_karcher_mean.py
+++ python/helpers/positive_definite_karcher_mean.py
@@ -1,11 +1,24 @@
 from pymanopt.manifolds.psd import PositiveDefinite
 from pymanopt.solvers.nelder_mead import compute_centroid
 
+import numpy as np
 
-def positive_definite_karcher_mean(A):
+
+def positive_definite_karcher_mean(A: np.ndarray):
     """Compute Karcher means using pymanopt.
-       Inspired in https://www.manopt.org/reference/examples/positive_definite_karcher_mean.html#_subfunctions
-       A =
+
+    Inspired in https://www.manopt.org/reference/examples/positive_definite_karcher_mean.html#_subfunctions
+
+    Parameters
+    ----------
+    A: np.ndarray
+        of shape (n, n, m)
+
+    Returns
+    -------
+    np.ndarray
+        m Karcher means
+
     """
     n = A.shape[0]
     assert n == A.shape[1], "The slices of A must be square"
