4373df2c9fc1806e676da06edf207cf6d9e36746
natlachaman <nati_269@hotmail.com>
2020-05-16 18:50:41 +0200

    starter code for viz

--- python/main.py
+++ python/main.py
@@ -1,20 +1,55 @@
 import os
+import logging
+import matplotlib.pyplot as plt
 from mne.io.eeglab.eeglab import read_raw_eeglab
 
 from .clean_artifacts import clean_artifacts
 
 
 class EEG:
-    def __int__(self, filepath=""):
+    def __init__(self, filepath=""):
         self._filepath = filepath
         if ~os.path.exists(filepath):
+            logging.info("Couldn't find file. Loading .set file in data/output instead...")
             self._filepath = os.path.join("data", "output", "sme_1_1.xdf_filt.set")
-
-    def load(self, preload=True):
-        return read_raw_eeglab(self._filepath, preload=preload)
+        self.data = read_raw_eeglab(self._filepath, preload=True)
+        self.channel_criterion = .85
+        self.line_noise_criterion = 4
+        self.burst_criterion = 5
+        self.window_criterion = .25
+        self.highpass = (.25,.75)
+        self.channel_criterion_max_bad_time = .5
+        self.burst_criterion_ref_max_bad_chns = 0.075
+        self.burst_criterion_ref_tolerances = (-3.5, 5.5)
+        self.window_criterion_tolerances = (-3.5, 7)
+        self.flatline_criterion = 5
+        self.nolocs_channel_criterion = .45
+        self.noloc_channel_criterion_excluded = .1
+        self.clean_data = None
 
     def clean(self):
-        return clean_artifacts()
+        self.clean_data = clean_artifacts(signal=self.data,
+                                           channel_criterion=self.channel_criterion,
+                                           line_noise_criterion=self.line_noise_criterion,
+                                           burst_criterion=self.burst_criterion,
+                                           window_criterion=self.window_criterion,
+                                           highpass=self.highpass,
+                                           channel_criterion_max_bad_time=self.channel_criterion_max_bad_time,
+                                           burst_criterion_ref_max_bad_chns=self.burst_criterion_ref_max_bad_chns,
+                                           burst_criterion_ref_tolerances=self.burst_criterion_ref_tolerances,
+                                           window_criterion_tolerances=self.window_criterion_tolerances,
+                                           flatline_criterion=self.flatline_criterion,
+                                           nolocs_channel_criterion=self.nolocs_channel_criterion,
+                                           noloc_channel_criterion_excluded=self.noloc_channel_criterion_excluded)
 
     def visualize(self):
-        pass
+        # simple visualization
+        C, S = self.data.shape
+        fig, ax = plt.subplots(C, 1)
+        ax = ax.ravel()
+
+        # plot each channels separately
+        for c, ax_i in enumerate(ax):
+            ax_i.plot(self.data[c, :], color="b")
+            if self.clean_data is not None:
+                ax_i.plot(self.clean_data[c, :], color="r")
